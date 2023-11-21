392620b6cde4f4b3cd04dfe9ee98a1f6c68b7252
Brady Dukart <dukartbrady@gmail.com>
2023-11-20 22:15:35 -0600

    removes unnecessary handling of isMobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
-import { createContext, useEffect, useState } from "react";
 import { Flex, useMediaQuery } from "@chakra-ui/react";
 import { Sidebar } from "./components/Sidebar";
 import { MobileHeader } from "./components/MobileHeader";
 import { Tasks } from "./pages/Tasks";
 
-export const MobileContext = createContext(false);
-
 function App() {
-	const [isMobile, setIsMobile] = useState(false);
-	const [isSmallerThan992] = useMediaQuery("(max-width: 992px)");
-	console.log("isSmallerThan992", isSmallerThan992);
-	console.log(MobileContext);
-	console.log("isMobile", isMobile);
-	useEffect(() => {
-		setIsMobile(isSmallerThan992);
-	}, [isSmallerThan992, setIsMobile]);
+	const [isMobile] = useMediaQuery("(max-width: 992px)");
+
 
 	return (
-		<MobileContext.Provider value={isMobile}>
-			<Flex
-				bgColor="gray.700"
-				h="100vh"
-				w="100vw"
-				position="relative"
-				direction={["column", null, null, "row"]}
-			>
-				{isMobile ? <MobileHeader /> : <Sidebar />}
-				<Tasks isMobile={isMobile} />
-			</Flex>
-		</MobileContext.Provider>
+		<Flex
+			bgColor="gray.700"
+			h="100vh"
+			w="100vw"
+			position="relative"
+			direction={["column", null, null, "row"]}
+		>
+			{isMobile ? <MobileHeader /> : <Sidebar />}
+			<Tasks isMobile={isMobile} />
+		</Flex>
 	);
 }
 
